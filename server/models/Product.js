d99ed4ba6d4fa6c848ef2f47f083bb3fa6568adc
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-05-02 18:05:09 -0600

    added product model

---
+++ server/models/Product.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const productSchema = new Schema({
+    category: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    image: {
+        type: String,
+        required: false
+    },
+    productId: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+});
+
+const Product = model('Product',productSchema);
+
+module.exports = Product;
