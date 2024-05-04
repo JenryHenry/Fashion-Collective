aaa69bb8424adc8fe7a15568dcb21cae5ec57093
April Vasquez <apri1mayrain@users.noreply.github.com>
2024-05-04 16:56:20 -0500

    Update homepage and navbar
    Homepage has logo graphic to welcome user to site.
    Navbar will show different tabs depending on users login status.
    App.css will override default Radix theme and is used for custom color palette.
    Removed all bootstrap dependencies from package.json.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,56 @@
 import { Box, TabNav } from '@radix-ui/themes';
+import { Link, useLocation } from 'react-router-dom';
+
+import AuthService from '../utils/auth';
 
 const Navbar = () => {
-  return (
-    <Box>
-        <TabNav.Root>
-            <TabNav.Link href='/' active>Home</TabNav.Link>
-            <TabNav.Link href='/search'>Search</TabNav.Link>
-            {/* "My Outfits" link should only display if user is logged in */}
-            <TabNav.Link href='/my-outfits'>My Outfits</TabNav.Link>
-            <TabNav.Link href='/cart'>Cart</TabNav.Link> 
-            {/* If user is logged in, should display "Logout" */}
-            <TabNav.Link href='/login'>Login / Signup</TabNav.Link>
-        </TabNav.Root>
-    </Box>
-  );
+  let pathname = window.location.pathname;
+  let { state } = useLocation();
+
+  // If user is logged in, navbar tabs are: Home, Search, My Outfits, Cart, Logout
+  if(AuthService.loggedIn()){
+    return (
+      <Box>
+          <TabNav.Root>
+              <TabNav.Link asChild active={pathname === '/'}>
+                <Link to='/' state='/'>Home</Link>
+              </TabNav.Link>
+              <TabNav.Link asChild active={pathname === '/search'}>
+                <Link to='/search' state='/search'>Search</Link>
+              </TabNav.Link>
+              <TabNav.Link asChild active={pathname === '/my-outfits'}>
+                <Link to='/my-outfits' state='/my-outfits'>My Outfits</Link>
+              </TabNav.Link>
+              <TabNav.Link asChild active={pathname === '/cart'}>
+                <Link to='/cart' state='/cart'>Cart</Link>
+              </TabNav.Link> 
+              <TabNav.Link asChild active={pathname === '/logout'}>
+                <Link to='/logout' state='/logout'>Logout</Link>
+              </TabNav.Link>
+          </TabNav.Root>
+      </Box>
+    )
+  }
+
+  // If user is logged in, navbar tabs are: Home, Search, Cart, Login / Signup
+    return (
+      <Box>
+          <TabNav.Root>
+              <TabNav.Link asChild active={pathname === '/'}>
+                <Link to='/' state='/'>Home</Link>
+              </TabNav.Link>
+              <TabNav.Link asChild active={pathname === '/search'}>
+                <Link to='/search' state='/search'>Search</Link>
+              </TabNav.Link>
+              <TabNav.Link asChild active={pathname === '/cart'}>
+                <Link to='/cart' state='/cart'>Cart</Link>
+              </TabNav.Link> 
+              <TabNav.Link asChild active={pathname === '/login'}>
+                <Link to='/login' state='/login'>Login / Signup</Link>
+              </TabNav.Link>
+          </TabNav.Root>
+      </Box>
+    )
 };
 
 export default Navbar;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,24 @@
-import { Text } from '@radix-ui/themes';
+import { Flex } from '@radix-ui/themes';
+import { Link } from 'react-router-dom';
+import logo from '/logo.png';
 
 const Home = () => {
-    return (
-        <Text>Home page content</Text>
-    )
+  return (
+    <>
+    <Link to='/search'>
+    <Flex justify='center'>
+    <img
+      src={logo}
+      alt='Fashion Collective Logo'
+      style={{
+        width: '80%',
+        height: '80%',
+      }}
+    />
+    </Flex>
+    </Link>
+    </>
+  );
 };
 
 export default Home;
