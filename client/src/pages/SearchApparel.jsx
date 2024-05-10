65af9e65d3149ad3fc87424c2ccd670a836c7b62
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-05-09 21:29:07 -0600

    removed unneccessary code

--- client/src/pages/SearchApparel.jsx
+++ client/src/pages/SearchApparel.jsx
@@ -38,35 +38,6 @@ const SearchApparel = () => {
         setSearch(event.target.value)
     };
   
-//     const [addAccessories] = useMutation(ADD_ACCESSORIES);
-//     const [addTop] = useMutation(ADD_TOP);
-
-//     const handleAddTop = async () => {
-//         const top = '663afcdf22e65882a09b3d6f';
-//         const outfitName = 'outfit8';
-//         try {
-//             const { data } = await addTop({
-//                 variables: { outfitName, top }
-//             })
-//         }
-
-
-//         catch (err) {
-//             console.log(err);
-//         }
-//     };
-
-//     const handleAddAccessories = async () => {
-//         const accessories = '663afcdf22e65882a09b3d7d';
-//         const outfitName = 'outfit8';
-//         try {
-//             const { data } = await addAccessories({
-//                 variables: { outfitName, accessories }
-//             })
-//         } catch (err) {
-//             console.log(err);
-//         }
-//     };
 
     // loading screen until product data is returned
     if(loading){
@@ -103,11 +74,7 @@ const SearchApparel = () => {
                 <Product productData={productData}/>
             </Grid>
         </Container>
-//         <div>
-//                 <p>Search Apparel page here</p>
-//                 <button onClick={handleAddTop}>Add Top</button>
-//                 <button onClick={handleAddAccessories}>Add Watch</button>
-//         </div>
+
         </>
     )
 };
