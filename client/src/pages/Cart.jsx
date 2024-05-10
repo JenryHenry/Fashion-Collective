3d22760b37f608b6a79ed2a0bf56bb05a13dad30
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-05-10 00:28:08 -0600

    removed toggle cart

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -4,7 +4,7 @@ import { useLazyQuery } from '@apollo/client';
 import { QUERY_CHECKOUT } from '../utils/queries';
 import { idbPromise } from '../utils/helpers';
 import { useStoreContext } from '../utils/GlobalState';
-import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../utils/actions';
+import { ADD_MULTIPLE_TO_CART } from '../utils/actions';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx'); 
 
