ab6b637af43472fca80edf741bdbf64076e28e05
Itzel Hernandez <itzel.hdz208@gmail.com>
2024-05-10 00:35:01 -0600

    added cart item

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -6,7 +6,7 @@ import { idbPromise } from '../utils/helpers';
 import { useStoreContext } from '../utils/GlobalState';
 import { ADD_MULTIPLE_TO_CART } from '../utils/actions';
 import Auth from '../utils/auth';
-import CarItem from '../components/CartItem';
+import CartItem from '../components/CartItem';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx'); 
 
