69c55252ee8b1f73243a4821861f8b68e882e290
April Vasquez <apri1mayrain@users.noreply.github.com>
2024-05-11 00:24:46 -0500

    Update renders of products
    With new local state in SearchBar, we can notify user if their search was not successful

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -16,61 +16,60 @@ const Product = () => {
         idbPromise('cart', 'put', { ...item });
     };
 
-    // Render message to user if no categories have been selected or no search performed
+    // Render message to user if no categories have been selected or no search peformed (upon inital page load)
     if(!state.products.length && !state.searchQuery){
         return(
-            <>
             <Box as='div' align='center'>
                 <Text>Please use the search bar to search for clothing item or click a clothing category above.</Text>
             </Box>
-            </>
         );
     }
 
-    // Render message to user if no clothing items were found
-    if(!state.products.length && state.searchQuery)
-    return(
-        <Box as='div' align='center'>
-            <Text>No items were found! Please try a different search.</Text>
-        </Box>
-    );
-
-    // Render products
-    if(state.products.length){
-        return (
-            <>
-            {state.products.map((product) => (
-            <Box key={product._id} maxWidth='350px'>
-                <Card variant='surface' size='3'>
-                    <Inset clip='border-box' side='top' pb='current'>
-                    <img
-                        src={'./images/' + product.image}
-                        alt={product.description}
-                        style={{
-                        display: 'block',
-                        objectFit: 'cover',
-                        width: '100%',
-                        height: 350,
-                        }}
-                    />
-                    </Inset>
-                    <Text as='p' size='5' weight='bold'> {product.title}
-                    </Text>
-                    <Text as='p' size='3' color='gray'> {product.category.name}
-                    </Text>
-                    <Text as='p' size='3' color='gray'> ${product.price}
-                    </Text>
-                    <Box align='center'>
-                        <Button onClick={() => handleAddToCart(product)}>
-                            <ShoppingCartOutlinedIcon /> Add to Cart
-                        </Button>
-                    </Box>
-                </Card>
+    // Render message to use if no products were found
+    if(!state.products.length && (state.searchQuery || state.currentCategory)){
+        return(
+            <Box as='div' align='center'>
+                <Text>No clothes found! Please try a different search.</Text>
             </Box>
-            ))}
-            </>
         );
     }
+
+    // Default - render products
+    return(
+        <>
+        {
+            state.products.map((product) =>
+                <Box key={product._id} maxWidth='350px'>
+                    <Card variant='surface' size='3'>
+                        <Inset clip='border-box' side='top' pb='current'>
+                        <img
+                            src={'./images/' + product.image}
+                            alt={product.description}
+                            style={{
+                            display: 'block',
+                            objectFit: 'cover',
+                            width: '100%',
+                            height: 350,
+                            }}
+                        />
+                        </Inset>
+                        <Text as='p' size='5' weight='bold'> {product.title}
+                        </Text>
+                        <Text as='p' size='3' color='gray'> {product.category.name}
+                        </Text>
+                        <Text as='p' size='3' color='gray'> ${product.price}
+                        </Text>
+                        <Box align='center'>
+                            <Button onClick={() => handleAddToCart(product)}>
+                                <ShoppingCartOutlinedIcon /> Add to Cart
+                            </Button>
+                        </Box>
+                    </Card>
+                </Box>
+            )
+        }
+        </>
+    );
 };
 
 export default Product;
